a9a940ba16e08d5f8f7c5a929c2fa603d577da2d
Steven Laff <steve.laff@gmail.com>
2019-10-26 14:49:45 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,8 @@
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
-  (1, "Mr. Chocolate", 4, "Male", "Brown", "Friendly", "TRUE"),
-  (2, "Rowdy", 9, "Female", "Brown", "Rowdy", "TRUE"),
-  (3, "Tabitha", 14, "Female", "Black", "Lazy", "FALSE"),
-  (4, "Sergeant Brown", "Male", "White", "Brave", "TRUE"),
-  (5, "Melissa", 2, "Female", "Black", "Curious", "TRUE"),
-  (6, "Grinch", 15, "Male", "Green", "Grouchy", "FALSE"),
-  (7, "Wendy", 5, "Female", "Brown", "Hungry", "TRUE"),
-  (8, null, 7, "Female", "Brown", "Protective", "FALSE");
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 4, "Male", "Brown", "Friendly", "TRUE");
+INSERT INTO bears VALUES (2, "Rowdy", 9, "Female", "Brown", "Rowdy", "TRUE");
+INSERT INTO bears VALUES (3, "Tabitha", 14, "Female", "Black", "Lazy", "FALSE");
+INSERT INTO bears VALUES (4,"Sargeant Brown", 8, "Male", "White", "Brave", "TRUE");
+INSERT INTO bears VALUES (5, "Melissa", 2, "Female", "Black", "Curious", "TRUE");
+INSERT INTO bears VALUES (6, "Grinch", 15, "Male", "Green", "Grouchy", "FALSE");
+INSERT INTO bears VALUES (7, "Wendy", 5, "Female", "Brown", "Hungry", "TRUE");
+INSERT INTO bears VALUES (8, null, 7, "Female", "Brown", "Protective", "FALSE");
