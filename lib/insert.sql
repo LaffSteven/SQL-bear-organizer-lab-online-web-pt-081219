9a83b40ffc8f4c64bb16923d4dcdc77b4897fdf8
Steven Laff <steve.laff@gmail.com>
2019-10-26 14:11:20 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES  ("Mr. Chocolate", 4, "Male", "Brown", "Friendly", "Alive"),
+          ("Rowdy", 9, "Female", "Brown", "Rowdy", "Alive"),
+          ("Tabitha", 14, "Female", "Black", "Lazy", "Dead"),
+          ("Sergeant Brown", "Male", "White", "Brave", "Alive"),
+          ("Melissa", 2, "Female", "Black", "Curious", "Alive"),
+          ("Grinch", 15, "Male", "Green", "Grouchy", "Dead"),
+          ("Wendy", 5, "Female", "Brown", "Hungry", "Alive");
+
+INSERT INTO bears (age, gender, color, temperament, alive)
+  VALUES  (7, "Female", "Brown", "Protective", "Dead");
