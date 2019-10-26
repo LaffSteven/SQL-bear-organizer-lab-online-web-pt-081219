e84553c38469b5a4ba029eac53982a53b3637201
Steven Laff <steve.laff@gmail.com>
2019-10-26 14:41:03 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,9 @@
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
-  (1, "Mr. Chocolate", 4, "Male", "Brown", "Friendly", "Alive"),
-  (2, "Rowdy", 9, "Female", "Brown", "Rowdy", "Alive"),
-  (3, "Tabitha", 14, "Female", "Black", "Lazy", "Dead"),
-  (4, "Sergeant Brown", "Male", "White", "Brave", "Alive"),
-  (5, "Melissa", 2, "Female", "Black", "Curious", "Alive"),
-  (6, "Grinch", 15, "Male", "Green", "Grouchy", "Dead"),
-  (7, "Wendy", 5, "Female", "Brown", "Hungry", "Alive"),
-  (8, null, 7, "Female", "Brown", "Protective", "Dead");
+  (1, "Mr. Chocolate", 4, "Male", "Brown", "Friendly", 1),
+  (2, "Rowdy", 9, "Female", "Brown", "Rowdy", 1),
+  (3, "Tabitha", 14, "Female", "Black", "Lazy", 0),
+  (4, "Sergeant Brown", "Male", "White", "Brave", 1),
+  (5, "Melissa", 2, "Female", "Black", "Curious", 1),
+  (6, "Grinch", 15, "Male", "Green", "Grouchy", 0),
+  (7, "Wendy", 5, "Female", "Brown", "Hungry", 1),
+  (8, null, 7, "Female", "Brown", "Protective", 0);
